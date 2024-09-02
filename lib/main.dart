36422095152d3cad8da67a41d1a106865ad35be3
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-09-02 09:55:13 -0500

    prgress

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -6,8 +8,15 @@ import 'package:production_planning/dependency_injection.dart';
 import 'package:production_planning/features/machines/presentation/bloc/machine_types_bloc/machine_types_bloc.dart';
 import 'package:production_planning/features/main_page/presentation/pages/main_page.dart';
 
+void logMessage(String message) {
+  final logFile = File('log.txt');
+  logFile.writeAsStringSync(message + '\n', mode: FileMode.append);
+}
+
 void main() async {
+  logMessage('App started');
   await initDependencies();
+  logMessage('dependencies started');
   runApp(const MyApp());
 }
 

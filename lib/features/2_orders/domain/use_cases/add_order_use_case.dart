d63a31c508feab83a49183899e65573a2c8a07bf
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-09-11 00:55:45 -0500

    Quick change, changed to List<NewOrderRequestModel>

--- lib/features/2_orders/domain/use_cases/add_order_use_case.dart
+++ lib/features/2_orders/domain/use_cases/add_order_use_case.dart
@@ -4,10 +4,10 @@ import 'package:production_planning/core/use_cases/use_case.dart';
 import 'package:production_planning/features/2_orders/domain/entities/order_entity.dart';
 import 'package:production_planning/features/2_orders/domain/request_models/new_order_request_model.dart';
 
-class AddOrderUseCase implements UseCase<OrderEntity, NewOrderRequestModel>{
+class AddOrderUseCase implements UseCase<OrderEntity, List<NewOrderRequestModel>>{
 
   @override
-  Future<Either<Failure, OrderEntity>> call({required NewOrderRequestModel p}) {
+  Future<Either<Failure, OrderEntity>> call({required List<NewOrderRequestModel> p}) {
     // TODO: implement call
     throw UnimplementedError();
   }

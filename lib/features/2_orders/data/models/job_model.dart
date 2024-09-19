e96b3f65c7db16cc047a4d7d42f899f7a6343c56
Laura Mendieta <laumendieta06@gmail.com>
2024-09-18 21:01:53 -0500

    [CREATE] data of orders in process

---
+++ lib/features/2_orders/data/models/job_model.dart
@@ -0,0 +1,37 @@
+import 'package:production_planning/features/1_sequences/domain/entities/sequence_entity.dart';
+import 'package:production_planning/features/2_orders/domain/entities/job_entity.dart';
+
+class JobModel {
+  final int jobId;
+  final int sequenceId;
+  final int amount;
+  final DateTime dueDate;
+  final int priority;
+
+  JobModel(
+      this.jobId, this.sequenceId, this.amount, this.dueDate, this.priority);
+
+  factory JobModel.fromJson(Map<String, dynamic> json) {
+    return JobModel(
+      json['job_id'],
+      json['sequence_id'],
+      json['amount'],
+      DateTime.parse(json['due_date']),
+      json['priority'],
+    );
+  }
+
+  JobEntity toEntity() {
+    // Aquí deberías tener un método para obtener la `SequenceEntity`
+    // a partir del `sequenceId`, por ahora simplemente lo omito para el ejemplo.
+    SequenceEntity sequence = SequenceEntity(sequenceId, [], 'Sequence Name');
+
+    return JobEntity(
+      jobId,
+      sequence,
+      amount,
+      dueDate,
+      priority,
+    );
+  }
+}
---
+++ lib/features/2_orders/data/repositories/order_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:production_planning/core/errors/failure.dart';
+import 'package:production_planning/features/2_orders/data/dao_interfaces/order_dao.dart';
+import 'package:production_planning/features/2_orders/data/dao_interfaces/job_dao.dart';
+import 'package:production_planning/features/2_orders/domain/entities/order_entity.dart';
+import 'package:production_planning/features/2_orders/domain/entities/job_entity.dart';
+
+class OrderRepositoryImpl {
+  final OrderDao orderDao;
+  final JobDao jobDao;
+
+  OrderRepositoryImpl({required this.orderDao, required this.jobDao});
+
+  Future<Either<Failure, List<OrderEntity>>> getAllOrders() async {
+    try {
+      // 1. Obtener todas las órdenes
+      final orderModels = await orderDao.getAllOrders();
+      List<OrderEntity> orders = [];
+
+      for (var orderModel in orderModels) {
+        // 2. Obtener todos los trabajos asociados a la orden
+        final jobModels = await jobDao.getJobsByOrderId(orderModel.orderId!);
+
+        // 3. Convertir los modelos a entidades
+        List<JobEntity> jobs =
+            jobModels.map((jobModel) => jobModel.toEntity()).toList();
+
+        // 4. Crear la entidad de orden y agregarla a la lista
+        orders.add(orderModel.toEntity(jobs));
+      }
+
+      return Right(orders);
+    } on Failure catch (error) {
+      return Left(error);
+    }
+  }
+}

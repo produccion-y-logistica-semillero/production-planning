7b5836a50bfe939cebb83315ab98d798fd5768ce
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2025-02-24 01:07:45 -0500

    Parallel machines migrated to using classes

--- lib/services/adapters/parallel_machine_adapter.dart
+++ lib/services/adapters/parallel_machine_adapter.dart
@@ -4,13 +4,11 @@ import 'package:production_planning/entities/metrics.dart';
 import 'package:production_planning/entities/order_entity.dart';
 import 'package:production_planning/entities/planning_machine_entity.dart';
 import 'package:production_planning/entities/planning_task_entity.dart';
-import 'package:production_planning/services/algorithms/flow_shop.dart';
 import 'package:production_planning/services/algorithms/parallel_machine.dart';
 import 'package:production_planning/repositories/interfaces/machine_repository.dart';
 import 'package:production_planning/repositories/interfaces/order_repository.dart';
 import 'package:production_planning/services/adapters/metrics.dart';
 import 'package:production_planning/shared/functions/functions.dart';
-
 import '../../entities/machine_entity.dart';
 
 class ParallelMachineAdapter {
@@ -24,64 +22,69 @@ class ParallelMachineAdapter {
   });
 
   Future<Tuple2<List<PlanningMachineEntity>, Metrics>?> parallelMachineAdapter(int orderId, String rule) async {
-    OrderEntity? order;
+    //getting full order
     final responseOrder = await orderRepository.getFullOrder(orderId);
-    responseOrder.fold((f) {}, (or) => order = or);
+    OrderEntity? order = responseOrder.fold((f) =>null, (or) => or);
     if (order == null) return null;
 
-    int machineTypeid = order!.orderJobs![0].sequence!.tasks![0].machineTypeId;
-    List<MachineEntity> machineEntities = [];
+    //we get all machines from this machine type
+    int machineTypeid = order.orderJobs![0].sequence!.tasks![0].machineTypeId;
     final responseMachines = await machineRepository.getAllMachinesFromType(machineTypeid);
-    responseMachines.fold((f) {}, (m) => machineEntities = m);
-    if (machineEntities.isEmpty) return null;
+    List<MachineEntity>? machineEntities = responseMachines.fold((f)=>null, (machines) => machines);
+    if (machineEntities == null) return null;
 
-    String machineTypeName = "";
-    final responseTypeMachine = await machineRepository.getMachineTypeName(machineTypeid);
-    responseTypeMachine.fold((f) {}, (name) => machineTypeName = name);
+    //we create the input 
+    final List<ParallelInput> inputJobs = [];
+    for(final job in order.orderJobs!){
+      Map<int, Duration> durationsInMachines= {};
+      //we get the duration it would take on each machine and add it to the map
+      for(final machine in machineEntities){
+        final task = job.sequence!.tasks![0];
+        durationsInMachines[machine.id!] = ruleOf3(machine.processingTime, task.processingUnits);
+      }
+      inputJobs.add(ParallelInput(
+        job.jobId!, job.dueDate, job.priority, job.availableDate, durationsInMachines)
+      );
+    } 
 
-    final List<Tuple5<int, DateTime, int, DateTime, List<Duration>>> inputJobs = order!.orderJobs!
-        .map((job) => Tuple5<int, DateTime, int, DateTime, List<Duration>>(
-            job.jobId!,
-            job.dueDate,
-            job.priority,
-            job.availableDate,
-            job.sequence!.tasks!.map((task) => task.processingUnits).toList()))
-        .toList();
+    //we create an the empy input struct for machines
+    final Map<int, List<Tuple2<DateTime, DateTime>>> machines = {};
+    for(final machine in machineEntities){
+      machines[machine.id!] = [];
+    }
 
-    final List<Tuple2<int, List<Tuple2<DateTime, DateTime>>>> machines = machineEntities
-        .map((machine) => Tuple2<int, List<Tuple2<DateTime, DateTime>>>(machine.id!, []))
-        .toList();
-
+    //we get the output, the result of the algorithm
     final output = ParallelMachine(
-      order!.regDate,
+      order.regDate,
       Tuple2(START_SCHEDULE, END_SCHEDULE),
       inputJobs,
       machines,
       rule,
     ).output;
 
+    //we transform the output to planning machines
     final Map<int, List<PlanningTaskEntity>> machineTasksMap = {};
-
     for (var out in output) {
-      final job = order!.orderJobs!.firstWhere((job) => job.jobId == out.value1);
+      final job = order.orderJobs!.firstWhere((job) => job.jobId == out.jobId);
+      final jobSequence =job.sequence!;
       final task = PlanningTaskEntity(
-        sequenceId: job.sequence!.id!,
-        sequenceName: job.sequence!.name,
-        taskId: job.sequence!.tasks![0].id!,
+        sequenceId: jobSequence.id!,
+        sequenceName: jobSequence.name,
+        taskId: jobSequence.tasks![0].id!,
         numberProcess: 1,
-        startDate: out.value3,
-        endDate: out.value4,
-        retarded: out.value5 > Duration.zero,
+        startDate: out.startDate,
+        endDate: out.endDate,
+        retarded: out.dueDate.isBefore(out.dueDate),
         jobId: job.jobId!,
         orderId: orderId,
       );
 
-      if (!machineTasksMap.containsKey(out.value2)) {
-        machineTasksMap[out.value2] = [];
+      if (!machineTasksMap.containsKey(out.machineId)) {
+        machineTasksMap[out.machineId] = [];
       }
-      machineTasksMap[out.value2]!.add(task);
+      machineTasksMap[out.machineId]!.add(task);
     }
-    machineEntities.forEach((m)=>print(m.id));
+
     final List<PlanningMachineEntity> machinesResult = machineTasksMap.entries
         .map((entry) => PlanningMachineEntity(
             entry.key,
@@ -92,7 +95,7 @@ class ParallelMachineAdapter {
 
     final metrics = getMetricts(
       machinesResult,
-      output.map((out) => Tuple3(out.value3, out.value4, out.value6)).toList(),
+      output.map((out) => Tuple3(out.startDate, out.endDate, out.dueDate)).toList(),
     );
 
     return Tuple2(machinesResult, metrics);
--- lib/services/algorithms/parallel_machine.dart
+++ lib/services/algorithms/parallel_machine.dart
@@ -1,16 +1,33 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
+class ParallelInput {
+  final int jobId;
+  final DateTime dueDate;
+  final int priority;
+  final DateTime availableDate;
+  final Map<int, Duration> durationsInMachines;
+  
+  ParallelInput(this.jobId, this.dueDate, this.priority, this.availableDate, this.durationsInMachines);
+}
+
+class ParallelOutput {
+  final int jobId;
+  final int machineId;
+  final DateTime startDate;
+  final DateTime endDate;
+  final Duration delay;
+  final DateTime dueDate;
+  
+  ParallelOutput(this.jobId, this.machineId, this.startDate, this.endDate, this.delay, this.dueDate);
+}
+
 class ParallelMachine {
   final DateTime startDate;
   final Tuple2<TimeOfDay, TimeOfDay> workingSchedule;
-  List<Tuple5<int, DateTime, int, DateTime, List<Duration>>> inputJobs = [];
-  //  job id   | due date | priority | Available date | Times (durations for each machine)
-  List<Tuple2<int, List<Tuple2<DateTime, DateTime>>>> machines = [];
-  //  machine Id | Scheduling (list of start and end times)
-
-  List<Tuple6<int, int, DateTime, DateTime, Duration, DateTime>> output = [];
-  // job id | machine id | start date | end date | delay | due date
+  List<ParallelInput> inputJobs = [];
+  Map<int, List<Tuple2<DateTime, DateTime>>> machines = {};
+  List<ParallelOutput> output = [];
 
   ParallelMachine(
     this.startDate,
@@ -20,141 +37,91 @@ class ParallelMachine {
     String rule,
   ) {
     switch (rule) {
-      case "SPT":
-        sptRule();
-        break;
-      case "LPT":
-        lptRule();
-        break;
-      case "EDD":
-        eddRule();
-        break;
-      case "FCFS":
-        fcfsRule();
-        break;
-      case "MINSLACK":
-        minimumSlackRule();
-        break;
-      case "CR":
-        criticalRatioRule();
-        break;
+      case "SPT": sptRule(); break;
+      case "LPT": lptRule(); break;
+      case "EDD": eddRule(); break;
+      case "FCFS":fcfsRule();break;
+      case "MINSLACK": minslackRule(); break;
+      case "CR": crRule(); break;
     }
-
-    printOutput();
-    printMachineSchedule();
   }
 
-  // Reglas de asignación de trabajos
-  void sptRule() {
-    inputJobs.sort((a, b) {
-      double avgTimeA = a.value5.reduce((v1, v2) => v1 + v2).inMinutes / a.value5.length;
-      double avgTimeB = b.value5.reduce((v1, v2) => v1 + v2).inMinutes / b.value5.length;
-      return avgTimeA.compareTo(avgTimeB);
-    });
-    assignJobsToMachines();
+  void sptRule(){
+    return _schedule((a, b) => _averageProcessingTime(a).compareTo(_averageProcessingTime(b)));
   }
 
-  void lptRule() {
-    inputJobs.sort((a, b) {
-      double avgTimeA = a.value5.reduce((v1, v2) => v1 + v2).inMinutes / a.value5.length;
-      double avgTimeB = b.value5.reduce((v1, v2) => v1 + v2).inMinutes / b.value5.length;
-      return avgTimeB.compareTo(avgTimeA);
-    });
-    assignJobsToMachines();
+  void lptRule(){
+    return _schedule((a, b) => _averageProcessingTime(b).compareTo(_averageProcessingTime(a)));
   }
 
-  void eddRule() {
-    inputJobs.sort((a, b) => a.value2.compareTo(b.value2));
-    assignJobsToMachines();
+  void eddRule(){
+    return _schedule((a, b) => a.dueDate.compareTo(b.dueDate));
   }
 
-  void fcfsRule() {
-    inputJobs.sort((a, b) => a.value4.compareTo(b.value4));
-    assignJobsToMachines();
+  void fcfsRule(){
+    return _schedule((a, b) => a.availableDate.compareTo(b.availableDate));
   }
 
-  void minimumSlackRule() {
-    inputJobs.sort((a, b) {
-      Duration slackA = a.value2.difference(a.value4) - a.value5.reduce((v1, v2) => v1 + v2);
-      Duration slackB = b.value2.difference(b.value4) - b.value5.reduce((v1, v2) => v1 + v2);
-      return slackA.compareTo(slackB);
-    });
-    assignJobsToMachines();
+  void minslackRule(){
+    return  _schedule((a, b) => _slack(a).compareTo(_slack(b)));
   }
 
-  void criticalRatioRule() {
-    inputJobs.sort((a, b) {
-      double criticalRatioA = calculateCriticalRatio(a);
-      double criticalRatioB = calculateCriticalRatio(b);
-      return criticalRatioA.compareTo(criticalRatioB);
-    });
-    assignJobsToMachines();
+  void crRule(){
+    return _schedule((a, b) => _criticalRatio(a).compareTo(_criticalRatio(b)));
   }
 
-  // Cálculo del Critical Ratio para un trabajo
-  double calculateCriticalRatio(Tuple5<int, DateTime, int, DateTime, List<Duration>> job) {
-    DateTime dueDate = job.value2;
-    DateTime availableDate = job.value4;
-    Duration totalProcessingTime = job.value5.reduce((v1, v2) => v1 + v2);
-    Duration timeRemaining = dueDate.difference(availableDate);
-    double criticalRatio = timeRemaining.inHours / totalProcessingTime.inHours;
-
-    // Si el ratio crítico es negativo o 0, el trabajo está muy atrasado
-    return criticalRatio <= 0 ? double.negativeInfinity : criticalRatio;
+  void _schedule(int Function(ParallelInput, ParallelInput) comparator) {
+    inputJobs.sort(comparator);
+    _assignJobsToMachines();
   }
 
-  // Asignar los trabajos a las máquinas disponibles
-  void assignJobsToMachines() {
-    List<DateTime> machineAvailable = List.generate(machines.length, (_) => startDate);
+  double _averageProcessingTime(ParallelInput job) {
+    return job.durationsInMachines.values.fold(0, (sum, d) => sum + d.inMinutes) / job.durationsInMachines.length;
+  }
 
+  int _slack(ParallelInput job) {
+    int remainingMinutes = job.dueDate.difference(job.availableDate).inMinutes;
+    int totalProcessingTime = job.durationsInMachines.values.fold(0, (sum, d) => sum + d.inMinutes);
+    return remainingMinutes - totalProcessingTime;
+  }
+
+  double _criticalRatio(ParallelInput job) {
+    int remainingMinutes = job.dueDate.difference(job.availableDate).inMinutes;
+    int totalProcessingTime = job.durationsInMachines.values.fold(0, (sum, d) => sum + d.inMinutes);
+    return totalProcessingTime == 0 ? double.infinity : remainingMinutes / totalProcessingTime;
+  }
+
+  void _assignJobsToMachines() {
+    Map<int, DateTime> machineAvailable = {for (var id in machines.keys) id: startDate};
+    
     for (var job in inputJobs) {
-      int jobId = job.value1;
-      DateTime dueDate = job.value2;
-      DateTime availableDate = job.value4;
-      List<Duration> processingTimes = job.value5;
+      int jobId = job.jobId;
+      DateTime dueDate = job.dueDate;
+      DateTime availableDate = job.availableDate;
 
-      for (int i = 0; i < processingTimes.length; ) {
-        Duration processingTime = processingTimes[i];
-
-        int bestMachineIndex = 0;
-        DateTime earliestAvailable = machineAvailable[0];
-
-        for (int j = 1; j < machineAvailable.length; j++) {
-          if (machineAvailable[j].isBefore(earliestAvailable)) {
-            bestMachineIndex = j;
-            earliestAvailable = machineAvailable[j];
-          }
-        }
-
-        DateTime machineStartTime = availableDate.isAfter(earliestAvailable)
+      for (var entry in job.durationsInMachines.entries) {
+        int machineId = entry.key;
+        Duration processingTime = entry.value;
+        DateTime machineStartTime = availableDate.isAfter(machineAvailable[machineId]!)
             ? availableDate
-            : earliestAvailable;
-
-        machineStartTime = adjustForWorkingSchedule(machineStartTime);
-
-        DateTime endTime = machineStartTime.add(processingTime);
-        endTime = adjustEndTimeForWorkingSchedule(machineStartTime, endTime);
-
+            : machineAvailable[machineId]!;
+        
+        machineStartTime = _adjustForWorkingSchedule(machineStartTime);
+        DateTime endTime = _adjustEndTimeForWorkingSchedule(machineStartTime, processingTime);
         Duration delay = endTime.isAfter(dueDate) ? endTime.difference(dueDate) : Duration.zero;
-
-        machineAvailable[bestMachineIndex] = endTime;
-
-        machines[bestMachineIndex].value2.add(Tuple2(machineStartTime, endTime));
-
-        output.add(Tuple6(jobId, machines[bestMachineIndex].value1, machineStartTime, endTime, delay, job.value2));
-
-        break; // Asignar un trabajo a una máquina
+        
+        machineAvailable[machineId] = endTime;
+        machines[machineId]?.add(Tuple2(machineStartTime, endTime));
+        output.add(ParallelOutput(jobId, machineId, machineStartTime, endTime, delay, dueDate));
       }
     }
   }
 
-  // Ajustar el tiempo de inicio para cumplir con el horario laboral
-  DateTime adjustForWorkingSchedule(DateTime start) {
+  DateTime _adjustForWorkingSchedule(DateTime start) {
     TimeOfDay workingStart = workingSchedule.value1;
     TimeOfDay workingEnd = workingSchedule.value2;
 
-    if (start.hour < workingStart.hour ||
-        (start.hour == workingStart.hour && start.minute < workingStart.minute)) {
+    if (start.hour < workingStart.hour || (start.hour == workingStart.hour && start.minute < workingStart.minute)) {
       return DateTime(start.year, start.month, start.day, workingStart.hour, workingStart.minute);
     } else if (start.hour > workingEnd.hour || (start.hour == workingEnd.hour && start.minute > workingEnd.minute)) {
       return DateTime(start.year, start.month, start.day + 1, workingStart.hour, workingStart.minute);
@@ -162,38 +129,16 @@ class ParallelMachine {
     return start;
   }
 
-  // Ajustar el tiempo de finalización según el horario laboral
-  DateTime adjustEndTimeForWorkingSchedule(DateTime start, DateTime end) {
+  DateTime _adjustEndTimeForWorkingSchedule(DateTime start, Duration duration) {
     TimeOfDay workingStart = workingSchedule.value1;
     TimeOfDay workingEnd = workingSchedule.value2;
-
     DateTime endOfDay = DateTime(start.year, start.month, start.day, workingEnd.hour, workingEnd.minute);
+    DateTime endTime = start.add(duration);
 
-    if (end.isAfter(endOfDay)) {
-      Duration remainingTime = end.difference(endOfDay);
+    if (endTime.isAfter(endOfDay)) {
+      Duration remainingTime = endTime.difference(endOfDay);
       return DateTime(start.year, start.month, start.day + 1, workingStart.hour, workingStart.minute).add(remainingTime);
     }
-
-    return end;
-  }
-
-  // Imprimir el resultado de las asignaciones de trabajos
-  void printOutput() {
-    print("Resultado de la asignación de trabajos:");
-    for (var result in output) {
-      print(
-          'Job ${result.value1} | Machine ${result.value2} | Start: ${result.value3} | End: ${result.value4} | Delay: ${result.value5}');
-    }
-  }
-
-  // Imprimir el horario de las máquinas
-  void printMachineSchedule() {
-    print("Horarios de las máquinas:");
-    for (var machine in machines) {
-      print('Machine ${machine.value1} Schedule:');
-      for (var schedule in machine.value2) {
-        print('  Start: ${schedule.value1}, End: ${schedule.value2}');
-      }
-    }
+    return endTime;
   }
 }

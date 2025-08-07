aa3998d28486eb139809a2fd5dae736c73fc4c23
Santcar05 <castrogarzonsantiago@gmail.com>
2025-08-06 22:35:33 -0500

    Cambio de nombre Nueva Ruta de Proceso

--- lib/presentation/1_sequences/pages/sequences_page.dart
+++ lib/presentation/1_sequences/pages/sequences_page.dart
@@ -34,11 +34,15 @@ class _SequencesPageState extends State<SequencesPage> {
       create: (context) => GetIt.instance.get<SeeProcessBloc>(),
       child: Scaffold(
         appBar: getAppBar(),
-        body: BlocBuilder<SequencesBloc, SequencesState>(builder: (context, state) {
-          Widget machinesContent = const Center(child: CircularProgressIndicator());
+        body: BlocBuilder<SequencesBloc, SequencesState>(
+            builder: (context, state) {
+          Widget machinesContent =
+              const Center(child: CircularProgressIndicator());
 
-          if (state is SequencesInitialState) BlocProvider.of<SequencesBloc>(context).retrieveSequencesMachine();
-          if (state is SequencesMachineFailure) machinesContent = const Center(child: Text("Error fetching"));
+          if (state is SequencesInitialState)
+            BlocProvider.of<SequencesBloc>(context).retrieveSequencesMachine();
+          if (state is SequencesMachineFailure)
+            machinesContent = const Center(child: Text("Error fetching"));
           if (state.machines != null) {
             machinesContent = MachinesList(
               machineTypes: state.machines!,
@@ -60,10 +64,11 @@ class _SequencesPageState extends State<SequencesPage> {
                   _onSaveOrder(context, name);
                 } else {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Por favor ingresa un nombre para la secuencia')),
+                    const SnackBar(
+                        content: Text(
+                            'Por favor ingresa un nombre para la secuencia')),
                   );
                 }
-
               },
               machines: state.machines ?? [],
               nodeEditorKey: nodeEditorKey,
@@ -78,11 +83,11 @@ class _SequencesPageState extends State<SequencesPage> {
           return Row(
             children: [
               IconButton(
-                onPressed: ()=>printInfo(context,
-                  title: 'Secuencias',
-                  content: 'Una secuencia se refiere al proceso de fabricacion de un producto, aca se define la secuencia de maquinas por las que se debe pasar para la fabricacion, el orden representa pre requisitos, y en cada paso por una maquina, o "tarea" se especifica cuanto tiempo en promedio se requiere en esa maquina, por ejemplo, la produccion de pan:\n\nTarea 1: Maquina de mezclado, 20min\nTarea 2: Camara de reposo, 10 min\nTarea 3: Maquina divisora, 4 min\nTarea 5: Maquina de formado, 15 min\nTarea 6: Maquina de horneado, 1 hora\nTarea 7: Maquina de enfriado 40 min'
-                ),
-                icon: const Icon(Icons.info)),
+                  onPressed: () => printInfo(context,
+                      title: 'Secuencias',
+                      content:
+                          'Una secuencia se refiere al proceso de fabricacion de un producto, aca se define la secuencia de maquinas por las que se debe pasar para la fabricacion, el orden representa pre requisitos, y en cada paso por una maquina, o "tarea" se especifica cuanto tiempo en promedio se requiere en esa maquina, por ejemplo, la produccion de pan:\n\nTarea 1: Maquina de mezclado, 20min\nTarea 2: Camara de reposo, 10 min\nTarea 3: Maquina divisora, 4 min\nTarea 5: Maquina de formado, 15 min\nTarea 6: Maquina de horneado, 1 hora\nTarea 7: Maquina de enfriado 40 min'),
+                  icon: const Icon(Icons.info)),
               Expanded(
                 flex: 1,
                 child: Container(
@@ -100,12 +105,18 @@ class _SequencesPageState extends State<SequencesPage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             ButtonMode(
-                              callback: state.isNewOrder
-                                  ? () => BlocProvider.of<SequencesBloc>(context).useMode(false)
-                                  : () => BlocProvider.of<SequencesBloc>(context).useMode(true),
-                              labelText: state.isNewOrder ? "Ver Secuencias" : "Nueva Orden",
-                              icon: Icons.roller_shades_closed_outlined,
-                              horizontalPadding: 30),
+                                callback: state.isNewOrder
+                                    ? () =>
+                                        BlocProvider.of<SequencesBloc>(context)
+                                            .useMode(false)
+                                    : () =>
+                                        BlocProvider.of<SequencesBloc>(context)
+                                            .useMode(true),
+                                labelText: state.isNewOrder
+                                    ? "Ver Secuencias"
+                                    : "Nueva Ruta de proceso",
+                                icon: Icons.roller_shades_closed_outlined,
+                                horizontalPadding: 30),
                             const SizedBox(height: 20),
                           ],
                         ),
@@ -125,22 +136,24 @@ class _SequencesPageState extends State<SequencesPage> {
   void _onSaveOrder(BuildContext context, String name) {
     final nodes = nodeEditorKey.currentState?.getNodes() ?? [];
     final connections = nodeEditorKey.currentState?.getConnections() ?? [];
-    BlocProvider.of<SequencesBloc>(context).saveProcess(name, nodes, connections);
+    BlocProvider.of<SequencesBloc>(context)
+        .saveProcess(name, nodes, connections);
     showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text('Ruta de proceso creada'),
-      content: const Text('La ruta de proceso ha sido guardada exitosamente.'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-            GetIt.instance.get<SequencesBloc>().useMode(false);
-          },
-          child: const Text('Aceptar'),
-        ),
-      ],
-    ),
-  );
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Ruta de proceso creada'),
+        content:
+            const Text('La ruta de proceso ha sido guardada exitosamente.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              GetIt.instance.get<SequencesBloc>().useMode(false);
+            },
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
   }
 }

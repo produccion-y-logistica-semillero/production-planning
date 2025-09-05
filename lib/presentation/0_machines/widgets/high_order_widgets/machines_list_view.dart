502e0a999f778c58b8ce6935e33f6b0ce15b7b57
Julian Ramos <julianc.ramos21@gmail.com>
2025-09-05 13:45:14 -0500

    fix(machine): refresh machine list on add when expanded or collapsed

--- lib/presentation/0_machines/widgets/high_order_widgets/machines_list_view.dart
+++ lib/presentation/0_machines/widgets/high_order_widgets/machines_list_view.dart
@@ -8,22 +8,28 @@ import 'package:production_planning/presentation/0_machines/bloc/machines_bloc/m
 import 'package:production_planning/presentation/0_machines/widgets/low_order_widgets/add_machine_dialog.dart';
 import 'package:production_planning/presentation/0_machines/widgets/low_order_widgets/machine_display_tile.dart';
 
-class MachinesListView extends StatelessWidget{
-
+// Changed to StatefulWidget to manage list expanded state 
+class MachinesListView extends StatefulWidget {
   final List<MachineTypeEntity> machineTypes;
+  const MachinesListView({super.key, required this.machineTypes});
+  // Creates the mutable state for MachinesListView
+  @override
+  State<MachinesListView> createState() => _MachinesListViewState();
+}
 
-  const MachinesListView({
-    super.key, 
-    required this.machineTypes,
-  });
+class _MachinesListViewState extends State<MachinesListView> {
+  final Set<int> expandedTypes = {};
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
     return ListView.builder(
-      itemCount: machineTypes.length,
+      // Accessing machineTypes through widget since it is atate class
+      itemCount: widget.machineTypes.length,
       itemBuilder: (context, index) {
+       // Get the current machine type from the widget's machineTypes list
+      final machineType = widget.machineTypes[index];
         return Container(
           margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
           decoration: BoxDecoration(
@@ -42,7 +48,7 @@ class MachinesListView extends StatelessWidget{
             ),
           ),
           child: BlocProvider(
-            create: (context) => GetIt.instance.get<MachineBloc>(),
+            create: (_) => GetIt.instance.get<MachineBloc>(),
             child: BlocBuilder<MachineBloc, MachinesState>(
               builder: (context, state) {
                 List<Widget> children = [];
@@ -50,14 +56,7 @@ class MachinesListView extends StatelessWidget{
                   children = [const ListTile(title: Text("Loading..."))];
                 } else if (state is MachinesRetrievingError) {
                   children = [const ListTile(title: Text("Error loading"))];
-                } else if (state is MachinesRetrievingSuccess) {
-                  children = state.machines!.map(
-                    (machine) => MachineDisplayTile(
-                      machine,
-                      () => _deleteMachine(context, machine.id!),
-                    ),
-                  ).toList();
-                } else if (state is MachineDeletionSuccess || state is MachineDeletionError) {
+                } else if (state is MachinesRetrievingSuccess || state is MachineDeletionSuccess || state is MachineDeletionError) {
                   children = state.machines!.map(
                     (machine) => MachineDisplayTile(
                       machine,
@@ -76,14 +75,14 @@ class MachinesListView extends StatelessWidget{
                         borderRadius: BorderRadius.circular(12),
                         child: ExpansionTile(
                           title: Text(
-                            machineTypes[index].name,
+                            machineType.name,
                             style: TextStyle(
                               color: colorScheme.onSurface,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
                           subtitle: Text(
-                            machineTypes[index].description,
+                            machineType.description,
                             style: TextStyle(
                               color: colorScheme.onSurfaceVariant,
                             ),
@@ -98,11 +97,16 @@ class MachinesListView extends StatelessWidget{
                           collapsedBackgroundColor: colorScheme.surfaceContainerLow,
                           backgroundColor: colorScheme.surfaceContainerHigh,
                           onExpansionChanged: (value) {
-                            if (state is MachinesRetrievingSuccess) {
-                              BlocProvider.of<MachineBloc>(context).machinesExpansionCollapses();
-                            } else {
-                              BlocProvider.of<MachineBloc>(context).retrieveMachines(machineTypes[index].id!);
-                            }
+                            // Used setState with expandedTypes to handle UI expansion and trigger BLoC actions
+                            setState(() {
+                              if (value) {
+                                expandedTypes.add(machineType.id!);
+                                BlocProvider.of<MachineBloc>(context).retrieveMachines(machineType.id!);
+                              } else {
+                                expandedTypes.remove(machineType.id!);
+                                BlocProvider.of<MachineBloc>(context).machinesExpansionCollapses();
+                              }
+                            });
                           },
                           children: children,
                         ),
@@ -116,24 +120,18 @@ class MachinesListView extends StatelessWidget{
                           IconButton(
                             onPressed: () => _addNewMachine(
                               context,
-                              machineTypes[index].id!,
-                              machineTypes[index].name,
+                              machineType.id!,
+                              machineType.name,
                             ),
-                            icon: Icon(
-                              Icons.add,
-                              color: colorScheme.primary,
-                            ),
+                            icon: Icon(Icons.add, color: colorScheme.primary),
                           ),
                           IconButton(
                             onPressed: () => _deleteMachineType(
                               context,
-                              machineTypes[index].id!,
+                              machineType.id!,
                               index,
                             ),
-                            icon: Icon(
-                              Icons.delete,
-                              color: colorScheme.error,
-                            ),
+                            icon: Icon(Icons.delete, color: colorScheme.error),
                           ),
                         ],
                       ),
@@ -148,69 +146,69 @@ class MachinesListView extends StatelessWidget{
     );
   }
 
-  void _deleteMachineType(BuildContext context, int machineId, int index) async{
+  void _deleteMachineType(BuildContext context, int machineId, int index) async {
     await showDialog(
       context: context,
       builder: (dialogContext) {
         return AlertDialog(
-          icon: const Icon(Icons.dangerous, color: Colors.red,),
+          icon: const Icon(Icons.dangerous, color: Colors.red),
           title: const Text("¿Estas seguro?"),
           content: const Text("Si eliminas este tipo de maquina, todas las maquinas asociadas seran eliminadas, ¿deseas continuar?"),
           actions: [
             TextButton(
-              onPressed: ()=>Navigator.of(dialogContext).pop(), 
-              child: const Text("Cancelar")
-            ),
-            TextButton(
-              onPressed: (){
-                BlocProvider.of<MachineTypesBloc>(context).deleteMachineType(machineId, index);
-                Navigator.of(dialogContext).pop();
-              }, 
-              child: const Text("Eliminar")
-            )
-          ],
-        );
-      }
-    );
-  }
-
-  void _deleteMachine(BuildContext context, int machineId) async{
-    await showDialog(
-      context: context, 
-      builder: (dialogContext){
-        return AlertDialog(
-          icon:  const Icon(Icons.dangerous, color: Colors.red,),
-          title: const Text("¿Estas seguro de eliminar la maquina?"),
-          actions: [
-            TextButton(
-              onPressed: ()=>Navigator.of(dialogContext).pop(), 
+              onPressed: () => Navigator.of(dialogContext).pop(),
               child: const Text("Cancelar"),
             ),
             TextButton(
-              onPressed: (){
-                BlocProvider.of<MachineBloc>(context).deleteMachine(machineId);
+              onPressed: () {
+                BlocProvider.of<MachineTypesBloc>(context).deleteMachineType(machineId, index);
                 Navigator.of(dialogContext).pop();
-              }, 
-              child: const Text("Confirmar")
+              },
+              child: const Text("Eliminar"),
             ),
-
-          ]
+          ],
         );
-      }
+      },
     );
   }
 
-  void _addNewMachine(BuildContext context, int machineId, String machineTypeName) async{
+  void _deleteMachine(BuildContext context, int machineId) async {
+    await showDialog(
+      context: context,
+      builder: (dialogContext) {
+        return AlertDialog(
+          icon: const Icon(Icons.dangerous, color: Colors.red),
+          title: const Text("¿Estas seguro de eliminar la maquina?"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(dialogContext).pop(),
+              child: const Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () {
+                BlocProvider.of<MachineBloc>(context).deleteMachine(machineId);
+                Navigator.of(dialogContext).pop();
+              },
+              child: const Text("Confirmar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _addNewMachine(BuildContext context, int machineId, String machineTypeName) async {
+    // Create field controllers
     final TextEditingController controllerCapacity = TextEditingController();
     final TextEditingController controllerPreparation = TextEditingController();
     final TextEditingController controllerRestTime = TextEditingController();
     final TextEditingController controllerContinue = TextEditingController();
     final TextEditingController nameController = TextEditingController();
-    final TextEditingController availabilityDateTimeController = TextEditingController(); 
-    print("** ______________**");
+    final TextEditingController availabilityDateTimeController = TextEditingController();
+
     await showDialog(
-      context: context, 
-      builder: (dialogContext){
+      context: context,
+      builder: (dialogContext) {
         return AddMachineDialog(
           machineTypeName,
           nameController: nameController,
@@ -219,40 +217,58 @@ class MachinesListView extends StatelessWidget{
           restTimeController: controllerRestTime,
           continueController: controllerContinue,
           availabilityDateTimeController: availabilityDateTimeController,
-          addMachineHandle: ()async{
-            print("__________**_________Adding new machine with typeId: $machineId");
-            if(
-              controllerCapacity.text.length != 5 || 
-              controllerPreparation.text.length != 5 || 
-              controllerRestTime.text.length != 5 || 
+          addMachineHandle: () async {
+            // check fields
+            if (
+              controllerCapacity.text.length != 5 ||
+              controllerPreparation.text.length != 5 ||
+              controllerRestTime.text.length != 5 ||
               nameController.text.isEmpty ||
               controllerContinue.text.isEmpty ||
               availabilityDateTimeController.text.isEmpty
-            ){
-              //if not all the fields have been added then we show another dialog showing the message
-              //we could try to have a custom field in the same dialog that shows the message, but that would imply
-              //to handle state in the dialog, and I don't want to deal with that
-              print("___________________Not all fields have been filled");
+            ) {
+              // If they are not complete, display a warning dialog box
               await showDialog(
                 context: dialogContext,
-                builder: (subDialogContext){
+                builder: (subDialogContext) {
                   return const AlertDialog(
-                    icon: Icon(Icons.dangerous_outlined, color: Colors.red,),
-                    content: Text("Asegurese de llenar todos los campos correctamente"),
+                    icon: Icon(Icons.dangerous_outlined, color: Colors.red),
+                    content: Text("Asegúrese de llenar todos los campos correctamente"),
                   );
-                }
+                },
               );
+              return;
             }
-            //else if to check if the inputs are correct, for instance, no 12:85
-            else {
-              print("___________________Adding new machine with typeId: $machineId");
-              BlocProvider.of<MachineBloc>(context).addNewMachine(controllerCapacity.text, controllerPreparation.text,  controllerContinue.text, controllerRestTime.text, nameController.text, machineId, availabilityDateTimeController.text); 
-              Navigator.of(dialogContext).pop();
+
+            // If they are complete, add new machine
+            BlocProvider.of<MachineBloc>(context).addNewMachine(
+              controllerCapacity.text,
+              controllerPreparation.text,
+              controllerContinue.text,
+              controllerRestTime.text,
+              nameController.text,
+              machineId,
+              availabilityDateTimeController.text,
+            );
+
+            // close create machine dialog
+            Navigator.of(dialogContext).pop();
+
+            //Wait to make sure it has been processed correctly
+            await Future.delayed(const Duration(milliseconds: 200));
+
+            if (expandedTypes.contains(machineId)) {
+              BlocProvider.of<MachineBloc>(context).retrieveMachines(machineId);
+            } else {
+              setState(() {
+                expandedTypes.add(machineId);
+              });
             }
           },
         );
-      }
+      },
     );
   }
+
 
 }
